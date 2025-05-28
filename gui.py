8decbd07545f90b11a63253279dd42ca82ec2e07
Hlaxas <miguel.herrera.quintero17@gmail.com>
2025-05-27 22:11:34 -0500

    Funcion para registrar al usuario

--- gui.py
+++ gui.py
@@ -11,15 +11,15 @@ class Interfaz:
         self.root.title("Centro de Bienestar Comunitario")
         self.root.geometry("400x500")
         self.gestor = GestorUsuarios()  # Instancia del gestor de usuarios
-        self.nombre = tk.Entry(root, font=("Arial", 12))
-        self.edad = tk.Entry(root, font=("Arial", 12))
-        self.actividad = tk.StringVar()
-        self.actividad.set("Spinning")
-        self.clases = tk.Entry(root, font=("Arial", 12))
+        self.nombre = tk.Entry(root, font=("Arial", 12)) # Campo para el nombre del usuario
+        self.edad = tk.Entry(root, font=("Arial", 12))# Campo para la edad del usuario
+        self.actividad = tk.StringVar()# Variable para la actividad seleccionada
+        self.actividad.set("Spinning")# Valor por defecto de la actividad
+        self.clases = tk.Entry(root, font=("Arial", 12))# Campo para el número de clases
 
         self.crear_widgets()
 
-    def crear_widgets(self):
+    def crear_widgets(self):# Método para crear los widgets de la interfaz
         tk.Label(self.root, text="Nombre:", font=("Arial", 12)).pack(pady=5)
         self.nombre.pack(fill="x", padx=20)
 
@@ -36,3 +36,14 @@ class Interfaz:
         tk.Button(self.root, text="Modificaciones", font=("Arial", 12), command=self.abrir_modificaciones).pack(pady=5, fill="x", padx=50)
         tk.Button(self.root, text="Mostrar Reporte", font=("Arial", 12), command=self.mostrar_reporte_en_ventana).pack(pady=5, fill="x", padx=50)
         tk.Button(self.root, text="Salir", font=("Arial", 12), command=self.root.quit).pack(pady=10, fill="x", padx=50)
+
+    def registrar(self):
+        try:
+            if int(self.edad.get()) < 15:
+                messagebox.showwarning("Edad mínima", "La edad mínima para registrarse es 15 años.")
+                return
+            user = Usuario(self.nombre.get(), self.edad.get(), self.actividad.get(), self.clases.get())
+            self.gestor.guardar_usuario(user)
+            messagebox.showinfo("Registro exitoso", f"Usuario registrado correctamente. Total a pagar: ${user.valor_pagado}")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
